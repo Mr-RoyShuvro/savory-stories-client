972a7ae2d1b8f53db91931f030ae155ea5d363b8
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-12-19 22:08:12 +0600

    Updated featured section

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -5,22 +5,25 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className='featured-item text-white pt-32 pb-32 mb-32'>
-            <div className='max-w-screen-xl mx-auto'>
-                <SectionTitle
-                    subHeading="Check it out"
-                    heading="From Our Menu"
-                ></SectionTitle>
-            </div>
-            <div className='md:flex justify-center items-center gap-16 pt-12 px-72'>
-                <div>
-                    <img src={FeaturedImg} alt="" />
+        <div className='featured-item bg-fixed text-white mb-32 pt-32 pb-32'>
+            <div className='featured-bg'></div>
+            <div className='featured-content'>
+                <div className='max-w-screen-xl mx-auto'>
+                    <div className='w-3/4 md:w-4/12 mx-auto text-center pb-10'>
+                        <h3 className='text-[#D99904] text-xl pb-4'>---Check it out---</h3>
+                        <h1 className='text-white text-[40px] border-t-[#E8E8E8] border-t-4 pt-5 border-b-[#E8E8E8] border-b-4 pb-6 uppercase'>From Our Menu</h1>
+                    </div>
                 </div>
-                <div>
-                    <h4>March 20, 2023</h4>
-                    <h3 className='uppercase'>WHERE CAN I GET SOME?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolor quibusdam error unde molestiae nihil et consequuntur veniam sapiente, facilis rerum deserunt. Ab quam tenetur aspernatur fugiat magni saepe eos obcaecati, architecto illum quae, magnam provident, nostrum hic doloremque. Eos labore ea facilis quas aspernatur nam explicabo asperiores ab impedit?</p>
-                    <button className="btn btn-outline btn-primary">Primary</button>
+                <div className='md:flex justify-center items-center gap-16 pt-12 px-72 '>
+                    <div>
+                        <img className='' src={FeaturedImg} alt="" />
+                    </div>
+                    <div>
+                        <h4>March 20, 2023</h4>
+                        <h3 className='uppercase'>WHERE CAN I GET SOME?</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam dolor quibusdam error unde molestiae nihil et consequuntur veniam sapiente, facilis rerum deserunt. Ab quam tenetur aspernatur fugiat magni saepe eos obcaecati, architecto illum quae, magnam provident, nostrum hic doloremque. Eos labore ea facilis quas aspernatur nam explicabo asperiores ab impedit?</p>
+                        <button className="btn btn-outline btn-primary">Primary</button>
+                    </div>
                 </div>
             </div>
         </div>
