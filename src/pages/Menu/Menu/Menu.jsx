6e2a80c3667c1f367da53c421660b17b7cd35c09
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-12-21 22:18:24 +0600

    Customized cover using parallax

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import MenuImg from '../../../assets/menu/banner3.jpg'
+import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 
 const Menu = () => {
     return (
@@ -14,6 +15,19 @@ const Menu = () => {
                 title={"OUR MENU"}
                 subTitle={"Would you like to try a dish?"}
             ></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover
+                img={MenuImg}
+                title={"OUR MENU"}
+                subTitle={"Would you like to try a dish?"}
+            ></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover
+                img={MenuImg}
+                title={"OUR MENU"}
+                subTitle={"Would you like to try a dish?"}
+            ></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Parallax, Background } from 'react-parallax';
 
 const Cover = ({ img, title, subTitle }) => {
     return (
-        <div
-            className="hero h-[600px]"
-            style={{
-                backgroundImage: `url("${img}")`,
-            }}>
-            <div className="hero-overlay bg-opacity-60 h-80 w-3/4 "></div>
-            <div className="hero-content text-neutral-content text-center ">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold uppercase text-white">{title}</h1>
-                    <p className="mb-5 uppercase text-white">
-                        {subTitle}
-                    </p>
+        <Parallax
+            blur={{ min: -15, max: 15 }}
+            bgImage={img}
+            bgImageAlt="the dog"
+            strength={-300}
+        >
+            <div
+                className="hero h-[600px]"
+            >
+                <div className="hero-overlay bg-opacity-60 h-80 w-3/4 "></div>
+                <div className="hero-content text-neutral-content text-center ">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold uppercase text-white">{title}</h1>
+                        <p className="mb-5 uppercase text-white">
+                            {subTitle}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Parallax>
     );
 };
 
