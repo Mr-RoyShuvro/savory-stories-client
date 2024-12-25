829abb4b84639cb41bd1753c9c458e1fd23dd012
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-12-25 23:21:23 +0600

    Customized food card

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -6,7 +6,7 @@ const FoodCard = ({ item }) => {
     const { name, recipe, image, price } = item;
 
     return (
-        <div className="card bg-base-100  shadow-xl">
+        <div className="card bg-[#F3F3F3] shadow-lg rounded-none">
             <figure>
                 <img
                     className='w-full'
@@ -15,10 +15,9 @@ const FoodCard = ({ item }) => {
             </figure>
             <p className='absolute right-0 mr-5 mt-5 px-6 py-3 bg-[#111827] text-white'>${price}</p>
             <div className="card-body flex flex-col items-center">
-                <h2 className="card-title text-[#151515] text-2xl font-semibold">{name}</h2>
-                <p className='text-[#737373] text-base font-normal leading-[26px]'>{recipe}</p>
-                <div className="card-actions justify-end">
-                    {/* <button className="btn btn-primary">Add to Cart</button> */}
+                <h2 className="card-title text-[#151515] text-2xl font-semibold pb-2 pt-8">{name}</h2>
+                <p className='text-[#737373] text-base font-normal leading-[26px] pb-6'>{recipe}</p>
+                <div className="card-actions justify-end pb-8">
                     <BtnSecondary btnSecondaryText="Add to cart"></BtnSecondary>
                 </div>
             </div>
