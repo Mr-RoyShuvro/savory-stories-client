9611ed5a5284e3deb89cabba86cc641c0415194d
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-12-30 23:18:04 +0600

    Added sign up page using react hook form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import loginImg from '../../assets/others/authentication2.png'
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
 
 const SignUp = () => {
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => console.log(data)
+
+    // console.log(watch("name"))
+
     return (
         <div className="hero bg-white min-h-screen">
             <div className="hero-content flex-col lg:flex-row gap-40">
@@ -13,19 +22,30 @@ const SignUp = () => {
 
                 <div className="card bg-white max-w-lg shrink-0 shadow-2xl w-1/2">
                     <h1 className="text-5xl font-bold">Sign Up now!</h1>
-                    <form className="card-body">
+
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                        <div className="form-control pb-6">
+                            <label className="label">
+                                <span className="label-text text-[#444444] text-xl font-semibold pb-4">Name</span>
+                            </label>
+                            <input type="text" {...register("name", { required: true })} name='name' placeholder="Enter your name" className="input input-bordered border-[#D1A054] bg-white text-[#A1A1A1] text-base font-normal"  />
+                            {errors.name && <span className='text-red-600 font-semibold'>Name is required</span>}
+                        </div>
+
                         <div className="form-control pb-6">
                             <label className="label">
                                 <span className="label-text text-[#444444] text-xl font-semibold pb-4">Email</span>
                             </label>
-                            <input type="email" name='email' placeholder="Enter your email" className="input input-bordered border-[#D1A054] bg-white text-[#A1A1A1] text-base font-normal" required />
+                            <input type="email" {...register("email", { required: true })} name='email' placeholder="Enter your email" className="input input-bordered border-[#D1A054] bg-white text-[#A1A1A1] text-base font-normal" />
+                            {errors.email && <span className='text-red-600 font-semibold'>Email is required</span>}
                         </div>
+
                         <div className="form-control pb-10">
                             <label className="label">
                                 <span className="label-text text-[#444444] text-xl font-semibold pb-4">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="Enter your password" className="input input-bordered border-[#D1A054] bg-white text-[#A1A1A1] text-base font-normal" required />
-
+                            <input type="password" {...register("password", { required: true, minLength: 6, maxLength: 20})} name='password' placeholder="Enter your password" className="input input-bordered border-[#D1A054] bg-white text-[#A1A1A1] text-base font-normal" />
+                            {errors.password && <span className='text-red-600 font-semibold'>Password is't valid</span>}
                         </div>
 
 
